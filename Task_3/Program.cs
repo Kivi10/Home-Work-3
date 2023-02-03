fa9e7d817da4e5e62619a96e8e697389d9fa825e
Hamzat <hgeroev11@mail.com>
2023-02-04 01:45:42 +0300

    Выполнено 3 задание

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,1 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+double Promt(string? message)
+{
+    System.Console.Write(message + " - > ");
+    string? inputValue = Console.ReadLine();
+    double result = Convert.ToDouble(inputValue);
+    return result;
+}
+
+double Cub(double num)
+{
+    double count = 0;
+    double count1 = 1;
+    double result = 0;
+    while (count < num)
+    {
+        result = Math.Pow(count1, 3);
+        System.Console.Write(result + " ");
+        count1++;
+        count++;
+    }
+    return result;
+}
+
+double number = Promt("Введите число");
+double res = Cub(number);
